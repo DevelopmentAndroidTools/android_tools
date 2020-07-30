f3b86cb76396e517897853a32ec35e72df2bf708
Sourangshu Ghosh <sourangshug123@gmail.com>
2020-07-30 11:06:43 +0530

    Create deltaota.sh

---
+++ tools/deltaota.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# Copyright (C) 2019 Shivam Kumar Jha <jha.shivam3@gmail.com>
+#
+# Helper functions
+
+# Store project path
+PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null && pwd )"
+
+# Common stuff
+source "$PROJECT_DIR"/helpers/common_script.sh "y"
+
+if [ ! -f "$1" ] || [ ! -f "$2" ]; then
+    echo -e "Supply full OTA file and patch OTA file as arguements!"
+    exit 1
+fi
+
+# Dependencies check
+if [ ! -d "$PROJECT_DIR/tools/Firmware_extractor" ] || [ ! -d "$PROJECT_DIR/tools/extract-dtb" ] || [ ! -d "$PROJECT_DIR/tools/mkbootimg_tools" ]; then
+    [[ "$VERBOSE" != "n" ]] && echo -e "Cloning dependencies..."
+    bash $PROJECT_DIR/helpers/dependencies.sh > /dev/null 2>&1
+fi
+
+# Set these var's acc to last arg
+URL=$( realpath "${@: -1}" )
+FILE=${URL##*/}
+EXTENSION=${URL##*.}
+UNZIP_DIR=${FILE/.$EXTENSION/}
+
+if [[ "$VERBOSE" = "n" ]]; then
+    echo "Extracting images of full OTA"
+    bash "$PROJECT_DIR"/tools/Firmware_extractor/patcher.sh "$@" -o "$PROJECT_DIR"/working/"$UNZIP_DIR" > /dev/null 2>&1
+else
+    bash "$PROJECT_DIR"/tools/Firmware_extractor/patcher.sh "$@" -o "$PROJECT_DIR"/working/"$UNZIP_DIR" -v
+fi
+
+bash "$PROJECT_DIR"/tools/rom_extract.sh "$PROJECT_DIR"/working/"$UNZIP_DIR"
+rm -rf "$PROJECT_DIR"/working/"$UNZIP_DIR"
