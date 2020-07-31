2b9f2818d038e059017ed02da0755e76add33877
Sourangshu Ghosh <sourangshug123@gmail.com>
2020-07-31 13:16:52 +0530

    Create lists_sort_all.sh

---
+++ helpers/lists_sort_all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# Copyright (C) 2020 Sourangshu Ghosh
+#
+# Helper functions
+
+# Store lists path
+LISTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/lists/" >/dev/null && pwd )"
+
+tools_lists=`find $LISTS_DIR/ -type f -printf '%P\n' | sort | grep -v "overlays/comments/"`
+for list in $tools_lists; do
+    sort -u -o "$LISTS_DIR/$list" "$LISTS_DIR/$list"
+done
